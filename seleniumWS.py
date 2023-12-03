1f4db0d55c5499781468494d216bbb0bfbac4666
Aditya <awesomeaditya0@gmail.com>
2023-12-03 14:23:05 +0530

    Second Update

---
+++ seleniumWS.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
+url='https://quotes.toscrape.com/js/'
+driver = webdriver.Chrome()
+driver.get(url)
+while True:
+    rendered_html = driver.page_source
+    soup = BeautifulSoup(rendered_html, 'html.parser')
+
+    tag_list = soup.select('a.tag')
+    for tag in tag_list:
+        print(tag.text)
+    try:
+        next_link = driver.find_element(By.CSS_SELECTOR, 'li.next a')
+        next_link.click()
+    except:
+        break
+
+driver.quit()
